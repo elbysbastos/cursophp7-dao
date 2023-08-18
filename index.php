6e36710fb33163b3e636206e327f005a082681fd
Elbys Bastos <elbysbastos@gmail.com>
2023-08-17 22:36:47 -0300

    Métodos de Lista

--- index.php
+++ index.php
@@ -8,10 +8,21 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
+/* --- Carrega usuário pelo ID ----
 $root = new Usuario();
-
 $root->loadById(3);
+echo $root;*/
 
-echo $root;
+/* --- carrega uma lista de usuários ----
+$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+/* --- carregua uma lista pelo nome do usuário ---
+$search = Usuario::search("jo");
+echo json_encode($search); */
+
+$usuario = new Usuario();
+$usuario->login("João","!@#$");
+echo $usuario;
 
 ?>
